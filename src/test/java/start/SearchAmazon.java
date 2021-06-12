926d8b16003189ab57c9d76cf05157d68348a38b
Alperen <alperen.yildirim7@gmail.com>
2021-06-13 02:55:13 +0300

    Change of code in loop part

--- src/test/java/start/SearchAmazon.java
+++ src/test/java/start/SearchAmazon.java
@@ -16,19 +16,19 @@ public class SearchAmazon {
         System.out.println("Please type what you want to search in Amazon.com");
         String searchTerm = scan.nextLine();
 
-        WebDriver driver = BrowserFactory.getDriver("chrome");
+        WebDriver driver = BrowserFactory.getDriver("firefox");
         driver.get("https://www.amazon.com");
         WebElement searchBox = driver.findElement(By.id("twotabsearchtextbox"));
         searchBox.sendKeys(searchTerm);
         //instead of clicking on search button, hitting enter key this way:
         searchBox.sendKeys(Keys.ENTER);
         Thread.sleep(2000);
-        if(driver.findElements(By.xpath("//span[starts-with(text(),'No results for')]")).size()==1){
-            System.out.println("No results for '"+searchTerm+"'");
+        if (driver.findElements(By.xpath("//span[starts-with(text(),'No results for')]")).size() == 1) {
+            System.out.println("No results for '" + searchTerm + "'");
             driver.quit();
             return;
         }
-        int pageCount =0;
+        int pageCount = 0;
         int totalResults = 0;
         int whichResult = 0;
 
@@ -41,35 +41,34 @@ public class SearchAmazon {
             ArrayList<WebElement> results = (ArrayList<WebElement>) driver.findElements(By.xpath(xpathToResults));
             // I wanted to calculate the number of results without taking it from Amazon
             totalResults += results.size();
+
             pageCount++;
+
+            ArrayList<WebElement> linkElements = (ArrayList<WebElement>) driver.findElements(By.xpath(xpathToResults + "/../.."));
 
             for (int j = 0; j < results.size(); j++) {
                 WebElement eachResult = results.get(j);
                 //every image element has an attribute named "alt" which has an info about product or sth.
                 String resultTitle = eachResult.getAttribute("alt");
-                if(resultTitle.startsWith("Sponsored Ad")){
-                    totalResults--; //To exclude sponsored results from real search results
-                    continue;
-                }
+
                 // To get the link of the result, I picked up the parent of parent element of eachResult element by using dots below
-                WebElement linkElement = driver.findElement(By.xpath(xpathToResults+"/../.."));
-                String urlToResult = linkElement.getAttribute("href");
-                System.out.println((whichResult+1)+"."+resultTitle);
-                System.out.println("Link: "+urlToResult);
+                WebElement eachLinkElement = linkElements.get(j);
+                System.out.println((whichResult+1)+". "+ resultTitle);
+                String urlToResult = eachLinkElement.getAttribute("href");
+                System.out.println("Link: " + urlToResult);
                 whichResult++;
             }
             if (nextPageBtn.getAttribute("class").contains("a-disabled")) {
                 //Next is not clickable anymore, so there is no next page, time to break
                 break;
-            }
-            else{
+            } else {
                 nextPageBtn.click();
                 // To get nothing wrong with locating element till learning WebDriverWait
                 Thread.sleep(5000);
             }
         }
         System.out.println("total results number: " + totalResults);
-        System.out.println(pageCount +" pages of results were there in total");
+        System.out.println(pageCount + " pages of results were there in total");
         driver.quit();
 
     }
